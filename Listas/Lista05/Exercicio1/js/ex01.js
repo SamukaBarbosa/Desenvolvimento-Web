7c2eef63a1afed4e93cad3d7166b74b4610a736b
SamukaBarbosa <samuelbardosamarciel@gmail.com>
2024-05-31 22:41:39 -0300

    Lista 5

---
+++ Listas/Lista05/Exercicio1/js/ex01.js
@@ -0,0 +1,6 @@
+var nome = prompt("Insira o seu nome: ")
+var idade = Number(prompt("Insira a sua idade: "))
+var emprego = prompt("Seu emprego atual: ")
+var endereco = prompt("Insira o seu endereço: ")
+
+console.log(`Seu nome é: ${nome},/nVocê tem ${idade} anos /nseu emprego atual é: ${emprego} /ne mora no endereço: ${endereco}` )
