82681d1b2071bcd53c21471d70fc83e804b62fa6
SamukaBarbosa <samuelbardosamarciel@gmail.com>
2024-06-11 18:17:26 -0300

    Aula 7

---
+++ Aulas/Aula07/js/for.js
@@ -0,0 +1,17 @@
+var tarefas = ['Lavar roupa', 'Lavar louca', 'Lavar banheiro', 'Lavar quintal', 'Dar banho no cachorro']
+
+// for (let indice = 0; indice < tarefas.length; indice++) {
+//    console.log(tarefas[indice])
+// }
+
+for (var tarefa of tarefas) {
+    console.log(tarefa)
+}
+
+// tarefas.forEach(tarefa => {
+//    console.log(tarefa)
+// })
+
+
+
+
